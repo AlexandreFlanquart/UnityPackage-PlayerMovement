c25e17d5b45cdcfb093ba4cd46fa63a72497b0d2
AlexandreFlanquart <alexflanqu@gmail.com>
2025-11-21 14:15:07 +0100

    ♻️ refactor : add events to inputManager

--- Runtime/Scripts/Player/IPlayerMovement.cs
+++ Runtime/Scripts/Player/IPlayerMovement.cs
@@ -1,14 +1,23 @@
+using System;
 using UnityEngine;
 
 namespace MyUnityPackage.Controller
 {
     public interface IPlayerMovement
     {
+        /*
             public Vector2 MovementInput { get; set; }
             public Vector2 LookInput { get; set; }
             public bool IsCrounching { get; set; }
             public bool JumpPressed { get; set; }
             public bool IsSprinting { get; set; }
+        */
+
+        public event Action OnJumpEvent;
+        public event Action OnMoveEvent;
+        public event Action OnCrouchEvent;
+        public event Action OnLookEvent;
+        public event Action OnSprintEvent;
     }
 
 }
--- Samples/Scripts/Input/Player/PlayerMovementInput.cs
+++ Samples/Scripts/Input/Player/PlayerMovementInput.cs
@@ -1,11 +1,13 @@
+using System;
+using MyUnityPackage.Toolkit;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 namespace MyUnityPackage.Controller
 {
-    [DefaultExecutionOrder(-2)]
-    public class PlayerMovementInput : MonoBehaviour,IPlayerMovement, PlayerControls.IPlayerMovementActions
+    public class PlayerMovementInput : MonoBehaviour, IPlayerMovement, PlayerControls.IPlayerMovementActions
     {
+        /*
         public Vector2 MovementInput { get => movementInput;  set => movementInput = value; }
         public Vector2 LookInput { get => lookInput;  set => lookInput = value; }
         public bool IsCrounching { get => isCrounching;  set => isCrounching = value; }
@@ -17,17 +19,23 @@ namespace MyUnityPackage.Controller
         private bool isCrounching ;
         private bool jumpPressed ;
         private bool isSprinting ;
-
+    
         void LateUpdate()
         {
             JumpPressed = false;
         }
+*/
+        public event Action OnJumpEvent;
+        public event Action OnMoveEvent;
+        public event Action OnCrouchEvent;
+        public event Action OnLookEvent;
+        public event Action OnSprintEvent;
 
         void OnEnable()
         {
             if(PlayerInputManager.Instance.PlayerControls == null)
             {
-                Debug.Log("Players controls is not set !");
+                MUPLogger.Error("Players controls is not set !");
                 return;
             }
             PlayerInputManager.Instance.PlayerControls.PlayerMovement.Enable();
@@ -37,45 +45,41 @@ namespace MyUnityPackage.Controller
         {
             if(PlayerInputManager.Instance.PlayerControls == null)
             {
-                Debug.Log("Players controls is not set !");
+                MUPLogger.Error("Players controls is not set !");
                 return;
             }
             PlayerInputManager.Instance.PlayerControls.PlayerMovement.Disable();
             PlayerInputManager.Instance.PlayerControls.PlayerMovement.RemoveCallbacks(this);
         }
+
         public void OnCrouch(InputAction.CallbackContext context)
         {
-            if(context.performed)
-                IsCrounching = true;
-            else if(context.canceled)
-                IsCrounching = false;
+            MUPLogger.Info("Crouch input detected : " + context.performed);
+            OnCrouchEvent?.Invoke();
         }
 
         public void OnJump(InputAction.CallbackContext context)
         {
-            if(!context.performed)
-                return;
-
-            JumpPressed = true;
+            MUPLogger.Info("Jump input detected : " + context.performed);
+            OnJumpEvent?.Invoke();
         }
 
         public void OnLook(InputAction.CallbackContext context)
         {
-            LookInput = context.ReadValue<Vector2>();
+            MUPLogger.Info("Look input detected : " + context.performed);
+            OnLookEvent?.Invoke();
         }
 
         public void OnMovement(InputAction.CallbackContext context)
         {
-            MovementInput = context.ReadValue<Vector2>();
+            MUPLogger.Info("Move input detected : " + context.performed);
+            OnMoveEvent?.Invoke();
         }
 
         public void OnSprint(InputAction.CallbackContext context)
         {
-            if(context.performed)
-                IsSprinting = true;
-            else if(context.canceled)
-                IsSprinting = false;
-            
+            MUPLogger.Info("Sprint input detected : " + context.performed);
+            OnSprintEvent?.Invoke();       
         }
     }
 }
